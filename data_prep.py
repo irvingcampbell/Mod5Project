f4efa6e12ed6033ee2573c3dee2537f58a7c820e
Anil Onal <anilca@gmail.com>
2019-12-16 17:58:06 -0500

    updated data_prep.py with recoding and imputer

--- data_prep.py
+++ data_prep.py
@@ -25,68 +25,53 @@ def drop_allocations(data_frame):
             coltodrop.append(c)
     return data_frame.drop(coltodrop, axis = 1)
 
+# Function to recode the categorical features 
 def recode(data_frame):
-    data_frame = data_frame.replace({'FS': {1: 2, 2: 1},
-                                     'BATH': {1: 2, 2: 1},
-                                     'REFR': {1: 2, 2: 1},
-                                     'RNTM': {1: 2, 2: 1},
-                                     'RWAT': {1: 2, 2: 1, 9:0},
-                                     'RWATPR': {1: 2, 2: 1, 9:0},
-                                     'SATELLITE': {1: 2, 2: 1},
-                                     'SINK': {1: 2, 2: 1},
-                                     'SMARTPHONE': {1: 2, 2: 1},
-                                     'STOV': {1: 2, 2: 1},
-                                     'TABLET': {1: 2, 2: 1},
-                                     'TEL': {1: 2, 2: 1},
-                                     'KIT': {1: 2, 2: 1},
-                                     'PLM': {1: 2, 2: 1, 9: 0},
-                                     'PLMPRP': {1: 2, 2: 1, 9: 0},
-                                     'SRNT': {0: 2},
-                                     'SVAL': {0: 2},
-                                     'TEN': {1: 3, 2: 4, 3: 2, 4: 1},
-                                     'ELEFP': {1: 2, 2: 1},
-                                     'FULFP': {1: 2, 2: 1},
-                                     'GASFP': {1: 2, 2: 1}})
+    # Binary categories
+    dict_bin = {0: 1, 1: 2}
+    bin_cols = ['HUGCL', 'NPP', 'NR', 'PSF', 'R18']
+    for c in bin_cols:
+        data_frame[c] = data_frame[c].map(dict_bin)
+    dict_bin = {1: 2, 2: 1}
+    bin_cols = ['BROADBND', 'COMPOTHX', 'DIALUP', 'HISPEED', 'LAPTOP', 'OTHSVCEX', \
+                'SATELLITE', 'SMARTPHONE', 'TABLET', 'TEL', 'FS', 'BATH', 'REFR', \
+                'SINK', 'STOV', 'KIT', 'RNTM']
+    for c in bin_cols:
+        data_frame[c] = data_frame[c].map(dict_bin)
+    dict_bin = {0: 2, 1: 1}
+    bin_cols = ['SRNT', 'SVAL']
+    for c in bin_cols:
+        data_frame[c] = data_frame[c].map(dict_bin)
+    # Three categories
+    dict_bin = {1: 2, 2: 1, 9:0}
+    bin_cols = ['RWAT', 'RWATPR', 'PLM', 'PLMPRP', 'HOTWAT']
+    for c in bin_cols:
+        data_frame[c] = data_frame[c].map(dict_bin)
+    dict_bin = {1: 2, 2: 1, 3: 3}
+    bin_cols = ['ELEFP', 'FULFP', 'GASFP', 'WATFP']
+    for c in bin_cols:
+        data_frame[c] = data_frame[c].map(dict_bin)
     return data_frame
     
-# Imputer
+# Customized imputer function
+# Since fill_value is a constant, we apply the function to the entire dataset
 def imputer(data_frame):
-    data_frame = data_frame.drop(data_frame.FS.isna(), \
-                                 axis = 0)
-    data_frame = data_frame.drop(data_frame.HINCP.isna(), \
-                                 axis = 0)
-    data_frame['HINCP'] = data_frame.HINCP * \
-                          data_frame.ADJINC / (10**6) 
-    imptr = SimpleImputer(strategy = 'constant', fill_value = 99)
-    data_frame['WIF'] = imptr(data_frame.WIF)
-    data_frame['WORKSTAT'] = imptr(data_frame.WORKSTAT)
     imptr = SimpleImputer(strategy = 'constant', fill_value = 0)
-    data_frame['VEH'] = imptr(data_frame.VEH)
-    data_frame['BATH'] = imptr(data_frame.BATH)
-    data_frame['REFR'] = imptr(data_frame.REFR)
-    data_frame['SINK'] = imptr(data_frame.SINK)
-    data_frame['STOV'] = imptr(data_frame.STOV)
-    data_frame['KIT'] = imptr(data_frame.KIT)
-    data_frame['BDSP'] = imptr(data_frame.BDSP)
-    data_frame['BLD'] = imptr(data_frame.BLD)
-    data_frame['RWAT'] = imptr(data_frame.RWAT)
-    data_frame['RWATPR'] = imptr(data_frame.RWATPR)
-    data_frame['PLM'] = imptr(data_frame.PLM)
-    data_frame['PLMPR'] = imptr(data_frame.PLMPR)
-    data_frame['SRNT'] = imptr(data_frame.SRNT)
-    data_frame['SVAL'] = imptr(data_frame.SVAL)
-    data_frame['MV'] = imptr(data_frame.MV)
-    data_frame['TEN'] = imptr(data_frame.TEN)
-    data_frame['CONP'] = imptr(data_frame.CONP)
-    data_frame['ELEFP'] = imptr(data_frame.ELEFP)
-    data_frame['ELEP'] = imptr(data_frame.ELEP)
-    data_frame['FULFP'] = imptr(data_frame.FULFP)
-    data_frame['FULP'] = imptr(data_frame.FULP)
-    data_frame['GASFP'] = imptr(data_frame.GASFP)
-    data_frame['GASP'] = imptr(data_frame.GASP)
-    data_frame['HFL'] = imptr(data_frame.HFL)
-    data_frame = data_frame.drop(['ACR', 'AGS', 'FINCP', 'VALP', 'VACS'], axis = 1)
-    
+    relevant_cols = ['SERIALNO', 'REGION', 'DIVISION', 'ST', 'PUMA', 'FS', \
+                     'HINCP', 'ADJINC', 'WIF', 'WORKSTAT', 'VEH', 'NP', 'HHL', \
+                     'FPARC', 'HHT', 'HUGCL', 'HUPAC', 'LNGI', 'MULTG', 'NPF', \
+                     'NPP', 'NR', 'NRC', 'PARTNER', 'PSF', 'R18', 'R65', 'SSMC', \
+                     'ACCESS', 'BROADBND', 'COMPOTHX', 'DIALUP', 'HISPEED', 'LAPTOP', \
+                     'OTHSVCEX', 'SATELLITE', 'SMARTPHONE', 'TABLET', 'TEL', \
+                     'TYPE', 'BATH', 'BDSP', 'BLD', 'REFR', 'RMSP', 'RWAT', \
+                     'RWATPR', 'SINK', 'STOV', 'TEN', 'KIT', 'MV', 'PLM', 'PLMPRP', \
+                     'HOTWAT', 'SRNT', 'SVAL', 'YBL', 'CONP', 'ELEFP', 'ELEP', \
+                     'FULFP', 'FULP', 'GASFP', 'GASP', 'HFL', 'INSP', 'MHP', \
+                     'RNTM', 'RNTP', 'WATFP', 'WATP', 'GRNTP', 'SMOCP']
+    data_frame = data_frame[relevant_cols]
+    data_frame = data_frame.dropna(subset = ['FS', 'HINCP'])
+    data_frame = imptr.fit_transform(data_frame)
+    return data_frame
     
 # Import the nationwide data-it is presented in two files 
 file_dir = '/Users/flatironschol/FIS-Projects/Module5/data/'
